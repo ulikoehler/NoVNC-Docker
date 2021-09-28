6b52c1a30487c8becef2738349002ff4674a3e1b
Uli Köhler <ukoehler@techoverflow.net>
2021-09-28 17:36:29 +0200

    Bump NoVNC to 1.3.0-beta

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export NAME=ulikoehler/novnc
-export VERSION=1.2.0-1
+export VERSION=1.3.0-beta
 docker build -t ${NAME}:${VERSION} .
-docker build -t ${NAME}:latest .
+docker build -t ${NAME}:beta .
 docker push ${NAME}:${VERSION}
+docker push ${NAME}:beta
