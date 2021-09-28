70af1c7daf77a37dd47283f6a09022cd35689368
Uli Köhler <ukoehler@techoverflow.net>
2021-09-28 17:43:57 +0200

    Add separate dockerfiles for separate versions

--- upload.sh
+++ upload.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 export NAME=ulikoehler/novnc
-export VERSION=1.3.0-beta
-docker build -t ${NAME}:${VERSION} .
-docker build -t ${NAME}:beta .
-docker push ${NAME}:${VERSION}
+docker build -f Dockerfile-1.2.0 -t ${NAME}:1.2.0 -t ${NAME}:1.2 -t ${NAME}:stable .
+docker push ${NAME}:1.2
+docker push ${NAME}:1.2.0
+docker push ${NAME}:stable
+
+docker build -f Dockerfile-1.3.0 -t ${NAME}:1.3.0  -t ${NAME}:1.2 -t ${NAME}:beta .
+docker push ${NAME}:1.3
+docker push ${NAME}:1.3.0
 docker push ${NAME}:beta
+
+docker build -f Dockerfile-master -t ${NAME}:master .
+docker push ${NAME}:master
