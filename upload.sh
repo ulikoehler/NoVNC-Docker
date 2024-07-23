184be13049a272d437e2d918153b52e45f210200
Uli Köhler <ukoehler@techoverflow.net>
2024-07-23 17:56:42 +0200

    Update to newest versions

--- upload.sh
+++ upload.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 export NAME=ulikoehler/novnc
-docker build -f Dockerfile-1.2.0 -t ${NAME}:1.2.0 -t ${NAME}:1.2 -t ${NAME}:stable .
+docker build -f Dockerfile-1.2.0 -t ${NAME}:1.2.0 -t ${NAME}:1.2
 docker push ${NAME}:1.2
 docker push ${NAME}:1.2.0
 docker push ${NAME}:stable
 
-docker build -f Dockerfile-1.3.0 -t ${NAME}:1.3.0  -t ${NAME}:1.2 -t ${NAME}:beta .
+docker build -f Dockerfile-1.3.0 -t ${NAME}:1.3.0  -t ${NAME}:1.3
 docker push ${NAME}:1.3
 docker push ${NAME}:1.3.0
 docker push ${NAME}:beta
+
+docker build -f Dockerfile-1.4.0 -t ${NAME}:1.4.0  -t ${NAME}:1.4
+docker push ${NAME}:1.4
+docker push ${NAME}:1.4.0
+docker push ${NAME}:beta
+
+docker build -f Dockerfile-1.5.0 -t ${NAME}:1.5.0  -t ${NAME}:1.5 -t ${NAME}:stable .
+docker push ${NAME}:1.5
+docker push ${NAME}:1.5.0
+docker push ${NAME}:beta
+
 
 docker build -f Dockerfile-master -t ${NAME}:master .
 docker push ${NAME}:master
